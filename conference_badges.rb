0e2d0403e79878c9bc23053fcb2ccd425a06549f
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-02 19:59:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,9 @@ end
 def batch_badge_creator(attendees)
   return badges
 
-def assign_rooms(attendees)
-    array = []
-    attendees.each_with_index do |name, index|
-       array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
-    end
-    array
+ def assign_rooms(array)
+   new = []
+   array.each_with_index { |element, index| new.push("Hello, #{element}! You'll be assigned to room #{index + 1}!") }
+   return new
  end
   
