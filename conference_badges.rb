b6de32f461d30b290bd08ba05ae5727273a4c74e
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-02 19:48:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
 def assign_rooms(attendees)
   rooms = 1..7
   attendees.each do |room|
-    return "Hello, #{name}! You'll be assigned to room #{room}"
+    return "Hello, #{name}! You'll be assigned to room #{rooms}"
   end
 end
   
